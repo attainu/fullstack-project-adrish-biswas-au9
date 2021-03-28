28cc6a4bd37e56c1c09532f541793f64fbbd59ec
kishan-das-au9 <kishandas909@gmail.com>
2021-03-28 22:28:05 +0530

    userhandle

--- studio-ghibli-universe/src/components/AdminUpgradeRequestGridDisplay.js
+++ studio-ghibli-universe/src/components/AdminUpgradeRequestGridDisplay.js
@@ -43,6 +43,28 @@ const handle_reject = (item) => {
     body: JSON.stringify({ _id: item._id, status: "rejected" })
   });
 }
+
+const handle_user = (item) => {
+  alert('account changed back to user')
+  fetch(`${order_url}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+
+    body: JSON.stringify({ _id: item._id, status: "expired" })
+  });
+  console.log({ email: item.email })
+  fetch(`${user_edit_url}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+
+    body: JSON.stringify({ email: item.email, role: "user" })
+  });
+}
+
 const AdminUpgradeRequestGridDisplay = (props) => {
   const display = (upgrade_list) => {
 
@@ -53,32 +75,52 @@ const AdminUpgradeRequestGridDisplay = (props) => {
         //const upgradeRoute = '/upgrade/' + item.id + '#top';
         return (
           < >
-            <div className="card mb-3" style={{ maxWidth: '', backgroundColor: 'gray' }}>
+            <div className="card mb-3" style={{ maxWidth: '', backgroundColor: '#687693' }}>
               <div className="row no-gutters">
-                <div className="col-md-4">
+                <div className="col-md-3">
                 
                 <center><p className="card-text" ><small>{item.date}</small></p></center>
 
                 </div>
-                <div className="col-md-4">
+
+                <div className="col-md-3">
+                
+                {item.status=="accepted"?
+                  <center><p className="card-text" ><small>{item.expiration_date}</small></p></center>
+                  :
+                  null
+                }
+
+                </div>
+
+
+                <div className="col-md-3">
                 <center><p className="card-text" ><small>{item.email}</small></p></center>
 
                 </div>
 
-                <div className="col-md-4">
+                <div className="col-md-3">
                   
 
 
                 { item.status=='pending'?
                   <center>
                   <button onClick={() => { handle_accept(item) }} type="button" className="btn btn-success" style={{margin:'8px'}}><p className="card-text " ><small>Accept</small></p></button>
+
                 
                   <button onClick={() => { handle_reject(item) }} type="button" className="btn btn-danger" style={{margin:'8px'}}><p className="card-text " ><small>Reject</small></p></button>
                   </center>
                   : 
+                  item.status=="accepted"?
+                  <center>
+                  <button type="button"  className="btn btn-warning" disabled style={{margin:'8px'}}><p className="card-text " ><small>{item.status}</small></p></button>
+                  <button type="button" onClick={() => { handle_user(item) }} className="btn btn-light"  style={{margin:'8px'}}><p className="card-text " ><small>back to user</small></p></button>
+                  </center>
+                  :
                   <center>
                   <button type="button"  className="btn btn-warning" disabled style={{margin:'8px'}}><p className="card-text " ><small>{item.status}</small></p></button>
                   </center>
+
                   
 
                 }
@@ -99,15 +141,19 @@ const AdminUpgradeRequestGridDisplay = (props) => {
       <div className="shoppingcontainer" >
       <div className="card mb-3 " style={{ maxWidth: '', backgroundColor: '#1daeed' }}>
                         <div className="row no-gutters">
-                          <div className="col-md-4">
+                          <div className="col-md-3">
                           <div className="card-body">
                           <center><p>Date</p></center>
                           </div></div>
-                          <div className="col-md-4">
+                          <div className="col-md-3">
+                          <div className="card-body">
+                          <center><p>Expiry Date</p></center>
+                          </div></div>
+                          <div className="col-md-3">
                           <div className="card-body">
                           <center><p>email</p></center>
                           </div></div>
-                          <div className="col-md-4">
+                          <div className="col-md-3">
                           <div className="card-body">
                           <center><p>Status</p></center>
                           </div></div>
