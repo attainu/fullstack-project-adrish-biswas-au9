5eba5d6cbd6f0b21f6be353ebafdba18dfaf086c
Adrish <adrishlinkin@gmail.com>
2021-03-30 02:42:17 +0530

    Profile page ongoing again!

---
+++ studio-ghibli-universe/src/components/FollowingDisplay.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react';
+import './WatchlistlistDisplay.css';
+import { connect } from 'react-redux';
+import { following_delete } from '../actions/actionfile';
+import { HashLink } from 'react-router-hash-link';
+
+//const wishlist_url = "https://ghibli-json-server.herokuapp.com/wishlist";
+
+class FollowingDisplay extends Component {
+    handleSubmit = (following) => {
+        this.props.dispatch(following_delete(following))
+        // fetch(`${wishlist_url}/${wishlist.id}`, {
+        //     method: 'DELETE',
+        //     headers: {
+        //         'Content-type': 'application/json'
+        //     },
+        // });
+        alert("Removed from following!")
+    }
+    renderTable = ({ following }) => {
+        if (following) {
+
+            return following.map((item) => {
+
+
+                const route = '/user/' + item.user_id + '#top'
+                return (
+                    <>
+                        <div className="card mb-3" style={{ maxWidth: '', backgroundColor: '#687693', padding:'5px' }}>
+                        <div className="row no-gutters">
+
+                        <div className="col-md-3">
+                            <center><img className='wishlist_film' alt="movie_poster" src={item.movieimage}></img></center>
+                            </div>
+
+
+                            <div className="col-md-3" style={{paddingTop:'80px'}}>
+                            <center><h6><HashLink to={route}>{item.name}</HashLink></h6></center>
+                            </div>
+                            
+                            
+                            <div className="col-md-3" style={{paddingTop:'80px'}}>
+                            <center><h6>{item.date}</h6></center>
+                            </div>
+
+
+                            <div className="col-md-3" style={{paddingTop:'50px'}}>
+                            <center><button className="btn" onClick={() => { this.handleSubmit(item) }}><img alt='delete_bin' src="https://img.icons8.com/fluent/48/000000/filled-trash.png" /></button></center>
+                            </div>   
+
+                        </div></div>
+
+                    </>
+                )
+            })
+        }
+    }
+
+    render() {
+        let object = (<div></div>)
+        if (this.props.following) {
+            object = (this.props.following.length === 0) ?
+                (
+                    <div className='empty_wishlist'>
+                        <h5 style={{color:'wheat'}}>Follow a watchlist from the community to make it a part of your Ghibli Universe!</h5>
+                    </div>
+                ) :
+                (                <>
+                    <div className="card mb-3" style={{ maxWidth: '', backgroundColor: '#1daeed', padding:'10px' }}>
+                    <div className="row no-gutters">
+                      <div className="col-md-3">
+    
+                      <center>Movie</center>     
+                        </div>
+    
+                        <div className="col-md-3">
+    
+    
+                               <center>Movie name</center> 
+    
+                        </div>
+    
+                        <div className="col-md-3">
+    
+    
+                                
+                                <center>Date</center> 
+    
+                        </div>
+    
+                        <div className="col-md-3">
+    
+    
+                        <center>Delete</center> 
+    
+                        </div>
+
+                        </div>
+                        
+                    </div><div>
+    
+    {this.renderTable(this.props)}
+    
+    
+    </div>
+    </>)
+        }
+
+        return (
+            <div className="container" id="watchlist_display" style={{ paddingTop: '100px' }}>
+                <h4 style={{color:'#1daeed'}} className='wishlist_heading'>{sessionStorage.getItem('name')}'s Watchlist</h4>
+                { object }
+            </div >
+            
+        )
+    }
+
+}
+
+export default connect()(FollowingDisplay);
---
+++ studio-ghibli-universe/src/container/Following.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import WatchlistDisplay from '../components/WatchlistDisplay';
+import { following } from '../actions/actionfile';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
+import SideBar from '../components/SideBar'
+import './Unimain.css'
+//import axios from 'axios';
+//const wishlist_url = "https://ghibli-json-server.herokuapp.com/wishlist";
+
+class Following extends Component {
+    // constructor() {
+    //     super()
+
+    //     this.state = {
+    //         wishlist: ''
+    //     }
+    // }
+    componentDidMount() {
+        //const response = await axios.get(wishlist_url);
+        this.props.dispatch(following());
+
+
+        //this.setState({ wishlist: filtering });
+
+
+        // this.setState({ wishlist: response.data })
+        //console.log(response.data)
+    }
+    render() {
+        if (sessionStorage.getItem('email') === null) {
+            this.props.history.push('/')
+        }
+        let filtering='';
+        if (this.props.following) {
+            filtering = this.props.following.filter((item) => {
+                return sessionStorage.getItem('email') === item.email
+            })
+        }
+        return (
+            <>
+            <div className="container">
+                <WatchlistDisplay following={filtering} />
+            </div></>
+        )
+    }
+
+}
+
+function mapStateToProps(state) {
+    console.log(state.following)
+    return {
+        following: state.following
+    }
+
+}
+
+export default connect(mapStateToProps)(Following);
